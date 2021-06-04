e6760b5b2d1e737e38d8749cd3dda322c6334c81
JuliaByl <julia.bylund.97@gmail.com>
2021-06-04 22:36:41 +0000

    attach empty functions related to gameplay

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,3 +4,36 @@ if ($(window).width() <= 772) {
 } else {
     $('.hamburger-menu')[0].remove(); 
 }
+
+/*before game starts*/
+function pickTheme {}
+
+function catsTheme {}
+
+function plantsTheme {}
+
+function covidTheme {}
+
+function startGame {}
+
+function restartGame {}
+
+/*during game per level*/
+
+function flipCard {}
+
+function hideCard {}
+
+function matchCard {}
+
+function levelClear {}
+
+function stopTimer {}
+
+/*after a level is cleared*/
+
+function nextLevel {}
+
+/*after all levels are cleared*/
+
+
